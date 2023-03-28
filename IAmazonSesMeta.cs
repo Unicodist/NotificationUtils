4b2b8ac3e296890d541b34336109e509e25ae4d3
Ashish Neupane <47056442+Unicodist@users.noreply.github.com>
2023-03-28 10:52:57 +0545

    Upgraded notification utils package

---
+++ IAmazonSesMeta.cs
@@ -0,0 +1,8 @@
+namespace NotificationUtils;
+
+public interface IAmazonSesMeta
+{
+    public string Key { get; set; }
+    public string Pass { get; set; }
+    public string Email { get; set; }
+}
---
+++ Services/AwsSesService.cs
@@ -0,0 +1,47 @@
+using Amazon.Runtime;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+
+namespace NotificationUtils.Services;
+
+public class AwsSesService
+{
+    private readonly IAmazonSesMeta _meta;
+
+    public AwsSesService(IAmazonSesMeta meta)
+    {
+        _meta = meta;
+    }
+
+    public async Task SendEmailAsync(IEnumerable<string> to, string subject, string body)
+    {
+        var credentials = new BasicAWSCredentials(_meta.Key, _meta.Pass);
+        using var client = new AmazonSimpleEmailServiceClient(credentials, Amazon.RegionEndpoint.APSouth1);
+        var request = new SendEmailRequest
+        {
+            Source = _meta.Email,
+            Destination = new Destination
+            {
+                ToAddresses = to.ToList()
+            },
+            Message = new Message
+            {
+                Subject = new Content(subject),
+                Body = new Body
+                {
+                    Html = new Content(body)
+                }
+            }
+        };
+
+        try
+        {
+            var response = await client.SendEmailAsync(request);
+            return;
+        }
+        catch(Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
